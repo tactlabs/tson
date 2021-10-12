e8874b3b716aa8da5c2e0fcb109605e9157832d9
Raja CSP Raman <raja.csp@gmail.com>
2021-10-12 15:36:57 -0400

    Simple test case added

--- tests/test_tson.py
+++ tests/test_tson.py
@@ -1,7 +1,7 @@
 """Tests for `tson` package."""
 
 import pytest
-
+import core
 
 
 def test_command_line_interface():
@@ -11,5 +11,14 @@ def test_command_line_interface():
     b = 3
 
     c = a + b
+
+    json_ = {
+        "one" : "two"
+    }
+
+    tson_ = core.convert_json_to_tson(json_)
+    # print(tson_)
+
+    tson_values = list(tson_['result'].values())
     
-    assert c == 5
+    assert ("two" in tson_values)
